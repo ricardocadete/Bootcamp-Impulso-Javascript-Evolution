8a4a5c0285ca01684989766df5481ab0bbb52182
Cadete <ricardobcadete@gmail.com>
2022-07-08 17:17:32 -0300

    desafios typescrit completos

---
+++ Projetos/desafios-typescript/solucao1.ts
@@ -0,0 +1,6 @@
+let employee = {
+    code: 0,
+    name: 'string'
+};
+employee.code = 10;
+employee.name = "John";
---
+++ Projetos/desafios-typescript/solucao2.ts
@@ -0,0 +1,29 @@
+interface Pessoa {
+    nome: string,
+    idade: number | string,
+    profissao: string
+}
+
+let pessoa1: Pessoa = {
+    nome: "maria",
+    idade: 29,
+    profissao: "atriz"
+};
+
+let pessoa2: Pessoa = {
+    nome: "roberto",
+    idade: 19,
+    profissao: "Padeiro"
+};
+
+let pessoa3: Pessoa = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+
+let pessoa4: Pessoa = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+};
